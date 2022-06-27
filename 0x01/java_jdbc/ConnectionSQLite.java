de2fbe0fa074dc58140435c2edb9009fdc9920bf
tc-anaalves <anaalves@ciandt.com>
2022-06-27 15:08:44 -0300

    ajuste

--- 0x01/java_jdbc/ConnectionSQLite.java
+++ 0x01/java_jdbc/ConnectionSQLite.java
@@ -10,25 +10,32 @@ public class ConnectionSQLite {
     private static String DB_NOME = "sqlite_database_2022.db";
 
     public static void main(String[] args) {
-        initConnection();
+        connection = initConnection();
+
+        closeConnection(connection);
     }
 
-    private static void initConnection() {
+    private static Connection initConnection() {
         try {
             Class.forName("org.sqlite.JDBC");
             connection = DriverManager.getConnection(String.format("jdbc:sqlite:%s", DB_NOME));
+            System.out.println("Conexão com o Banco de Dados <" + DB_NOME + "> Estabelecida.");
+            return connection;
         } catch (ClassNotFoundException e) {
             System.out.println("O driver expecificado nao foi encontrado.");
             Logger.getLogger(ConnectionSQLite.class.getName()).log(Level.SEVERE, null, e);
+            return null;
         } catch (SQLException e) {
             System.out.println("Nao foi possivel conectar ao Banco de Dados.");
             Logger.getLogger(ConnectionSQLite.class.getName()).log(Level.SEVERE, null, e);
+            return null;
         }
     }
 
-    public void closeConnection(Connection connection) {
+    public static void closeConnection(Connection connection) {
         try {
             connection.close();
+            System.out.println("Conexão com o Banco de Dados Encerrada.");
         } catch (SQLException ex) {
             Logger.getLogger(ConnectionSQLite.class.getName()).log(Level.SEVERE, null, ex);
         }
